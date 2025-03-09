8c34b20c5b97bdd88a5e78d7cb80aa3f0b2cc379
rickyzatnika <rickyzatnika91@gmail.com>
2025-03-09 07:51:26 +0700

    zod validation

--- auth.ts
+++ auth.ts
@@ -1,14 +1,13 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "./lib/db"
+import prisma from "./lib/db";
 
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub, Google],
+  providers: [Google],
   callbacks: {
     async session({ session, user }) {
       if (session.user) {
--- lib/zodSchema.tsx
+++ lib/zodSchema.tsx
@@ -3,10 +3,10 @@ import { z } from "zod";
 export const projectSchema = z.object({
     id: z.string().optional(), // Optional buat update
     title: z.string().min(1).max(50),
-    description: z.string().min(1).max(50),
+    description: z.string().min(1),
     imageUrl: z.string(),
     demoUrl: z.string(),
-    sourceCodeUrl: z.string(),
+    sourceCodeUrl: z.string().optional(),
     technologies: z.array(z.object({ name: z.string() })),
 });
 
